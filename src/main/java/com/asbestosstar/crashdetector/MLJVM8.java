1c9c28cbef971fc4cd9368a5a0315d065b9154e9
Asbestosstar <ricsch97@gmail.com>
2025-05-04 22:22:44 -0700

    Add files via upload

---
+++ src/main/java/com/asbestosstar/crashdetector/MLJVM8.java
@@ -0,0 +1,10 @@
+package com.asbestosstar.crashdetector;
+
+/**
+ * Esta clase existe porque beginScanning no existe con el desc mismo pero los params mismo. es en el nivel src.
+ */
+public class MLJVM8 {
+
+
+
+}
